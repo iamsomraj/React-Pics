e01d0765aa6ce1958ecda42aa905b89ed081a493
iamsomraj <iamsomraj@gmail.com>
2020-01-28 04:09:58 +0530

    Updates

---
+++ precache-manifest.cb0b5202652f0a2bb564634f8ea3439a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004a39b089ae0ba568ad14876d40155f",
+    "url": "/React-Pics/index.html"
+  },
+  {
+    "revision": "fe1be28e5666769a89ad",
+    "url": "/React-Pics/static/js/2.4a2a66c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-Pics/static/js/2.4a2a66c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a438f3f9ed1da987a55d",
+    "url": "/React-Pics/static/js/main.ff4a7d75.chunk.js"
+  },
+  {
+    "revision": "a809076e178425e85f2b",
+    "url": "/React-Pics/static/js/runtime-main.e269f149.js"
+  }
+]);
