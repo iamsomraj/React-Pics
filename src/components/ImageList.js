2bde5cf5c89f7e990facc351dba06a3c84857c56
iamsomraj <iamsomraj@gmail.com>
2020-01-28 16:13:05 +0530

    added grid via semantic ui

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -4,13 +4,18 @@ import React from 'react';
 const ImageList = props => {
   let ImageList = props.images.map(imageEl => {
     return (
-        <img className="ui image"
-          key={imageEl.id}
-          src={imageEl.urls.regular}
-        />
+      <div className="column">
+        <div className="ui segment">
+          <img
+            className="ui medium image"
+            key={imageEl.id}
+            src={imageEl.urls.regular}
+          />
+        </div>
+      </div>
     );
   });
-  return <div className="ui small images">{ImageList}</div>;
+  return <div className="ui equal width three column divided grid">{ImageList}</div>;
 };
 
 export default ImageList;
